9d2e5c2b508c5d74bc2ffd4a07685cc34f9c3157
sontrinh16 <trinhleson2000@gmail.com>
2023-08-28 14:22:52 +0700

    add upgrade handler

---
+++ module/app/upgrades/auction_module/handler.go
@@ -0,0 +1,44 @@
+package auction_module
+
+import (
+	auctionkeeper "github.com/Gravity-Bridge/Gravity-Bridge/module/x/auction/keeper"
+	auctiontypes "github.com/Gravity-Bridge/Gravity-Bridge/module/x/auction/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+var (
+	allowTokens = map[string]bool{
+		// USDC
+		"gravity0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": true,
+		// USDT
+		"gravity0xdAC17F958D2ee523a2206206994597C13D831ec7": true,
+	}
+	NextAuctionPeriodHeightMargin = uint64(5)
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	auctionKeeper *auctionkeeper.Keeper,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		ctx.Logger().Info("Starting upgrade...")
+
+		// Set params
+		defaultParams := auctiontypes.DefaultParams()
+
+		defaultParams.AuctionRate = sdk.NewDecWithPrec(2, 1)
+		defaultParams.AllowTokens = allowTokens
+
+		auctionKeeper.SetParams(ctx, defaultParams)
+
+		// Set EstimateNextAuctionPeriodHeight
+		auctionKeeper.SetEstimateAuctionPeriodBlockHeight(ctx, uint64(ctx.BlockHeight())+NextAuctionPeriodHeightMargin)
+
+		ctx.Logger().Info("Upgrade Complete")
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
